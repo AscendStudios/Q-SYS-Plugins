5500976033588b1d7a592339be8871d0cf5920ce
kcx1 <casey@ascendstudios.com>
2022-12-20 18:27:34 -0500

    Minor Updates

--- QSC/PageController/info.lua
+++ QSC/PageController/info.lua
@@ -2,7 +2,7 @@
 PluginInfo = {
   Name = "Page Navigation",
   Version = "1.0",
-  BuildVersion = "0.2.1.0",
+  BuildVersion = "0.2.1.3",
   Id = "PageNavigation",
   Author = "Casey Compton",
   Description = "Plugin for full page navigation within Q-SYS control devices"
